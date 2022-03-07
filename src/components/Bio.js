5d0dafa27bb34fbbe877bb7c12bf8a11d36e3ccb
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-03-07 06:28:18 -0700

    bio abbr / section : p refactor

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,8 +2,8 @@ import { HashLink } from "react-router-hash-link";
 
 const Bio = () => {
   return (
-    <ul>
-      <li className="bottom20">
+    <section>
+      <p className="bottom20">
         A seasoned musician and teacher, Shawn fell in love with programming in
         mid-2021 and never looked back. He loves intellectual challenges and
         completed the Full-Stack JavaScript{" "}
@@ -23,32 +23,33 @@ const Bio = () => {
           Team Treehouse
         </a>{" "}
         in December, 2021.
-      </li>
-      <li className="bottom20">
+      </p>
+      <p className="bottom20">
         As a musician, Shawn's been priveleged to appear with GRAMMY®-Award
         winners Doc Severinsen, Kenny Rogers, and Charlie Daniels, as well as
         the Knoxville Symphony Orchestra, Southern Stars Symphonic Brass, and
         Cathedral Brass. He was also seen on international television as a part
         of President George W. Bush's second innauguration parade.
-      </li>
-      <li className="bottom20">
+      </p>
+      <p className="bottom20">
         Shawn loves to make new things and is a prolific composer, having
         completed 9 unique works for tuba quartet, his ensemble of choice. His
         first published work, <em>Urban Suite</em>, was premiered at Eastman
-        School of Music in 2009. A finalist for the 2010 ITEA Harvey Phillips
-        Award, <em>Urban Suite</em> has been performed and recorded around the
-        world.
-      </li>
-      <li className="bottom20">
+        School of Music in 2009. A finalist for the 2010{" "}
+        <abbr title="International Tuba Euphonium Association">ITEA</abbr>{" "}
+        Harvey Phillips Award, <em>Urban Suite</em> has been performed and
+        recorded around the world.
+      </p>
+      <p className="bottom20">
         Shawn lives near Denver with his wife, flutist Whitney Kelley, two
         daughters, and English Springer Spaniel.
-      </li>
-      <li className="see">
+      </p>
+      <p className="see">
         <HashLink to={{ pathname: "/projects", hash: "#a" }}>
           see work &rarr;
         </HashLink>
-      </li>
-    </ul>
+      </p>
+    </section>
   );
 };
 
